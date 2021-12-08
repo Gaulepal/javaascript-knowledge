f3fb23e9dc6b5354f76b1dcae7a1d96d8341841f
gaulepal <gaulepal@gmail.com>
2021-12-08 13:14:13 +0100

    we can see that friend1 can call friend2 details using call() method

--- src/Apply-Call-Bind.js
+++ src/Apply-Call-Bind.js
@@ -10,11 +10,9 @@ let friend1 = {
 let friend2 = {
   name: 'Sita Ram',
   profession: 'Artist',
-  printDetail: function () {
-    document.body.innerHTML = this.name;
-  },
 };
 
-//call
-friend1.printDetail();
-friend2.printDetail();
+//*Here comes the concept of Call() function because as we notice that we're writing the same function printDetail() in two objects
+//when we use `this` keyword within the object then it points the same object
+//lets write the function in friend1 and access for both objects
+friend1.printDetail.call(friend2);
