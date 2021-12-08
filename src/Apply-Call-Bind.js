77d952ae1fde508c989d2e82e9ded0712133f7c4
gaulepal <gaulepal@gmail.com>
2021-12-08 13:05:25 +0100

    simple printDetail function is working

--- src/Apply-Call-Bind.js
+++ src/Apply-Call-Bind.js
@@ -1,21 +1,17 @@
 //data
-let friends = [
-  {
-    name: 'Rajan',
-    profession: 'Electrical Engineer',
-    printDetail: function () {
-      document.body.innerHTML = this.name;
-    },
+let friends = {
+  name: 'Rajan',
+  profession: 'Electrical Engineer',
+  printDetail: function () {
+    document.body.innerHTML = this.name;
   },
-  { name: 'Sita Ram', profession: 'Artist' },
-  { name: 'Dinesh', profession: 'Electrician' },
-  function printDetails() {
-    console.log('function is being called');
-  },
-];
+};
+
+//call
+friends.printDetail();
 
 //call
 // document.body.innerHTML = friends.map((person) => person.name);
 
 //call function
-document.body.innerHTML = friends.map((person) => person.printDetail());
+// document.body.innerHTML = friends.map((person) => person.printDetail());
