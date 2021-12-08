8c7ed3cb7a21569fce2a2f4fa62d5314bafa90b4
gaulepal <gaulepal@gmail.com>
2021-12-08 13:40:26 +0100

    bind() func needed to stored in variable and invoke when we need

--- src/Apply-Call-Bind.js
+++ src/Apply-Call-Bind.js
@@ -10,8 +10,8 @@ let friend2 = {
 };
 
 //function => now lets pass the city as parameter
-function printDetail(city) {
-  console.log(this, city);
+function printDetail(city, country) {
+  console.log(this, city, country);
 }
 
 //*Here comes the concept of Call() function because as we notice that we're writing the same function printDetail() in two objects
@@ -22,5 +22,15 @@ function printDetail(city) {
 
 //* now lets say we have function outside the object
 //we can call the function and pass the object
-printDetail.call(friend1.name, 'Chitwan');
+printDetail.call(friend1.name, 'Chitwan', 'Nepal');
 printDetail.call(friend2.name, 'Bharatpur');
+
+//lets use Apply() method instead of Call()
+printDetail.apply(friend1, ['Bharatpur', 'Nepal']); //here we can pass as an array
+
+//* lets see how we can work with Bind() func
+//basically bind is like call() func but we should not call it directly, we make a copy and call or invoke when we need
+
+//lets store in a variable and call it at the bottom => as we can see that we can store in variable and invoke when we need
+let funWithFriend = printDetail.bind(friend1.name, 'Pokhara', 'Nepal');
+funWithFriend();
