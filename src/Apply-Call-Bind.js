d105b301ed9e68142c2b4f822395b052efaeb638
gaulepal <gaulepal@gmail.com>
2021-12-08 13:23:20 +0100

    we can call the function and use Call() method

--- src/Apply-Call-Bind.js
+++ src/Apply-Call-Bind.js
@@ -2,9 +2,6 @@
 let friend1 = {
   name: 'Rajan',
   profession: 'Electrical Engineer',
-  printDetail: function () {
-    document.body.innerHTML = this;
-  },
 };
 //lets create another object
 let friend2 = {
@@ -12,8 +9,22 @@ let friend2 = {
   profession: 'Artist',
 };
 
+//function
+const printDetail = function () {
+  console.log(this.name);
+};
+
+function printFriend() {
+  console.log(this.profession);
+}
+
 //*Here comes the concept of Call() function because as we notice that we're writing the same function printDetail() in two objects
 //when we use `this` keyword within the object then it points the same object
 //lets write the function in friend1 and access for both objects
-friend1.printDetail.call(friend2.profession);
+// friend1.printDetail.call(friend2.profession);
 //this is also called function borrow
+
+//* now lets say we have function outside the object
+//we can call the function and pass the object
+printDetail.call(friend1);
+printFriend.call(friend1);
